0ab10cc7e134c6f5e0a54ceb97837555b4623bb3
mkapostolov <martin.apostolov@progress.com>
2019-12-04 13:55:26 +0200

    Remove debug log for Flex functions

--- index.js
+++ index.js
@@ -3,24 +3,10 @@ const handlers = require('./handlers/index');
 
 sdk.service({}, function(err, flex) {
   console.log(`===> Flex Service started (SDK v${flex.version})`);
-  console.log(process.versions);
+  console.log(`Node v${process.versions.node}`);
 
   // Flex functions
   for (var funcName in handlers) {
     flex.functions.register(funcName, handlers[funcName]);
   }
-
-  // Intercept and Log every call to a flex function
-  (function() {
-    var originalProcess = flex.functions.process;
-
-    flex.functions.process = function(task, modules, callback) {
-      console.log(`====> ${new Date().toISOString()}`);
-      console.log(`function: ${task.taskName}`);
-      console.log(`username: ${task.request.username}`);
-      console.log(`body: ${task.request.body}`);
-
-      originalProcess(task, modules, callback);
-    };
-  })();
 });
